9ce49e22d6cbae273e37d483d190657b8dac9849
Godwin Odenigbo <godwintrav@gmail.com>
2022-03-12 09:23:03 +0000

    added removeLesson event

--- src/App.vue
+++ src/App.vue
@@ -39,52 +39,7 @@
         </section>
 
         <section v-else id="check-out">
-            <div class="container">
-                <div v-if="(cartItems.length > 0)" class="row">
-
-
-                    <div  v-for="(cartItem, index) in cartItems" :key="cartItem.lessonID" class="col-md-4" style="margin-bottom: 20px;">
-                        <div class="col-md-12 check-out-1" >
-                            <div class="row">
-                                <div class="col-md-6 subject-box">
-                                    <p>Subject: {{ cartItem.lesson.topic }}<br> Location: {{ cartItem.lesson.location }}<br> Price: <span>&#163;{{ cartItem.lesson.price }}</span><br>  Icon: <span v-bind:class="cartItem.lesson.icon"></span> <br>  Quantity: <span > {{cartItem.space}}</span></p>
-                                </div>
-                                <div class="col-md-6">
-                                    <img v-bind:src="`https://cst3145-cw2-backend.herokuapp.com/${cartItem.lesson.image}`" alt=""
-                                        style="width: 100px; height: 100px; margin-top: 20px">
-                                </div>
-                            </div>
-                            <button v-on:click="removeLessonFromCart(index)" type="button" class="btn btn-1 btn-info" style="width: 100%">Remove from
-                                Cart</button>
-                        </div>
-                    </div>
-
-                   
-
-                </div>
-            </div>
-
-            <section id="check-out-form">
-                <div class="container">
-                    <div class="row check-out-row">
-                        <div class="col-md-12">
-                            
-                                <div class="row">
-                                    <div class="col">
-                                        <input v-on:keyup="validateRegexCheckOut" type="text" class="form-control" placeholder="Enter your full name" v-model="checkOutName" >
-                                    </div>
-                                    <div class="col">
-                                        <input v-on:keyup="validateRegexCheckOut" type="text" class="form-control" placeholder="Enter your mobile number" v-model="checkOutNumber" >
-                                    </div>
-                                </div>
-                                <div class="col-md-12 text-center">
-                                    <button v-bind:disabled="cannotCheckOut" v-on:click="checkOut" type="button" class="btn checkout-btn btn-info">Check Out</button>
-                                </div>
-                           
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Checkout :cartItems="cartItems" v-on:removeLesson="removeLessonFromCart"></Checkout>
             
         </section>
   </div>
@@ -92,11 +47,13 @@
 
 <script>
 import Lessons from './components/Lessons.vue'
+import Checkout from './components/Checkout.vue'
 
 export default {
   name: 'App',
   components: {
-    Lessons
+    Lessons,
+    Checkout
   },
   data() {
     return {
@@ -109,9 +66,7 @@ export default {
         showProduct: true,
         searchTxt: '',
         
-        checkOutName: '',
-        checkOutNumber: '',
-        cannotCheckOut: true
+        
     }
   },
   methods: {
@@ -177,52 +132,8 @@ export default {
                 return;
             }
             return this.lessons[lessonIndex];
-        },
-        validateRegexCheckOut: function () {
-            var nameRegexPattern = /^[a-zA-Z ]*$/;
-            var numberRegexPattern = /^\d+$/;
-            if (this.checkOutName.match(nameRegexPattern) && this.checkOutNumber.match(numberRegexPattern)) {
-                this.cannotCheckOut = false;
-            } else {
-                this.cannotCheckOut = true;
-            }
-        },
-        checkOut: function () {
-            fetch('https://cst3145-cw2-backend.herokuapp.com/collection/orders', {
-                method: 'POST', // set the HTTP method as 'POST'
-                headers: {
-                    'Content-Type': 'application/json', // set the data type as JSON
-                },
-                body: JSON.stringify({ name: this.checkOutName, phone: this.checkOutNumber, lessons: this.cartItems }), // need to stringify the JSON object
-            })
-                .then(response => response.json())
-                .then(responseJSON => {
-                    console.log('Success:', responseJSON);
-                });
-
-            for (let index = 0; index < this.cartItems.length; index++) {
-                const lesson = this.fetchLesson(this.cartItems[index].lessonID);
-                fetch(`https://cst3145-cw2-backend.herokuapp.com/collection/lessons/${lesson._id}`, {
-                    method: 'PUT', // set the HTTP method as 'POST'
-                    headers: {
-                        'Content-Type': 'application/json', // set the data type as JSON
-                    },
-                    body: JSON.stringify({ space: lesson.space}), // need to stringify the JSON object
-                })
-                    .then(response => response.json())
-                    .then(responseJSON => {
-                        console.log('Success:', responseJSON);
-                    });
-            }
-
-            this.$swal({
-                title: "Check out successful",
-                text: "Your order has been submitted",
-                icon: "success",
-            });
-            this.showProduct = true;
-            this.cartItems = [];
         }
+       
     },
     
     created: function () {
--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,5 +1,53 @@
 <template>
-    
+     <div>
+         <div class="container">
+                <div v-if="(cartItems.length > 0)" class="row">
+
+
+                    <div  v-for="(cartItem, index) in cartItems" :key="cartItem.lessonID" class="col-md-4" style="margin-bottom: 20px;">
+                        <div class="col-md-12 check-out-1" >
+                            <div class="row">
+                                <div class="col-md-6 subject-box">
+                                    <p>Subject: {{ cartItem.lesson.topic }}<br> Location: {{ cartItem.lesson.location }}<br> Price: <span>&#163;{{ cartItem.lesson.price }}</span><br>  Icon: <span v-bind:class="cartItem.lesson.icon"></span> <br>  Quantity: <span > {{cartItem.space}}</span></p>
+                                </div>
+                                <div class="col-md-6">
+                                    <img v-bind:src="`https://cst3145-cw2-backend.herokuapp.com/${cartItem.lesson.image}`" alt=""
+                                        style="width: 100px; height: 100px; margin-top: 20px">
+                                </div>
+                            </div>
+                            <button v-on:click="removeLesson(index)" type="button" class="btn btn-1 btn-info" style="width: 100%">Remove from
+                                Cart</button>
+                        </div>
+                    </div>
+
+                   
+
+                </div>
+            </div>
+
+            
+            <section id="check-out-form">
+                <div class="container">
+                    <div class="row check-out-row">
+                        <div class="col-md-12">
+                            
+                                <div class="row">
+                                    <div class="col">
+                                        <input v-on:keyup="validateRegexCheckOut" type="text" class="form-control" placeholder="Enter your full name" v-model="checkOutName" >
+                                    </div>
+                                    <div class="col">
+                                        <input v-on:keyup="validateRegexCheckOut" type="text" class="form-control" placeholder="Enter your mobile number" v-model="checkOutNumber" >
+                                    </div>
+                                </div>
+                                <div class="col-md-12 text-center">
+                                    <button v-bind:disabled="cannotCheckOut" v-on:click="checkOut" type="button" class="btn checkout-btn btn-info">Check Out</button>
+                                </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </section>
+     </div>
 </template>
 
 <script>
@@ -7,10 +55,61 @@ export default {
     name: 'Checkout',
     props: ['cartItems'],
     data() {
-        return {}
+        return {
+            checkOutName: '',
+        checkOutNumber: '',
+        cannotCheckOut: true
+        }
     },
     methods: {
-        
+        removeLesson: function(index){
+             this.$emit('removeLesson', index);
+        },
+         validateRegexCheckOut: function () {
+            var nameRegexPattern = /^[a-zA-Z ]*$/;
+            var numberRegexPattern = /^\d+$/;
+            if (this.checkOutName.match(nameRegexPattern) && this.checkOutNumber.match(numberRegexPattern)) {
+                this.cannotCheckOut = false;
+            } else {
+                this.cannotCheckOut = true;
+            }
+        },
+        checkOut: function () {
+            fetch('https://cst3145-cw2-backend.herokuapp.com/collection/orders', {
+                method: 'POST', // set the HTTP method as 'POST'
+                headers: {
+                    'Content-Type': 'application/json', // set the data type as JSON
+                },
+                body: JSON.stringify({ name: this.checkOutName, phone: this.checkOutNumber, lessons: this.cartItems }), // need to stringify the JSON object
+            })
+                .then(response => response.json())
+                .then(responseJSON => {
+                    console.log('Success:', responseJSON);
+                });
+
+            for (let index = 0; index < this.cartItems.length; index++) {
+                const lesson = this.cartItems[index].lesson;
+                fetch(`https://cst3145-cw2-backend.herokuapp.com/collection/lessons/${lesson._id}`, {
+                    method: 'PUT', // set the HTTP method as 'POST'
+                    headers: {
+                        'Content-Type': 'application/json', // set the data type as JSON
+                    },
+                    body: JSON.stringify({ space: lesson.space}), // need to stringify the JSON object
+                })
+                    .then(response => response.json())
+                    .then(responseJSON => {
+                        console.log('Success:', responseJSON);
+                    });
+            }
+
+            this.$swal({
+                title: "Check out successful",
+                text: "Your order has been submitted",
+                icon: "success",
+            });
+            this.showProduct = true;
+            this.cartItems = [];
+        }
     }
 }
 </script>
